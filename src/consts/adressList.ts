fb753b2a9ccd6664cf46dcabc1aaf600615d1e7d
AdnanCigtekin <cigtekinadnan@gmail.com>
2021-05-09 16:39:36 +0300

    fix: changed connected server

--- src/consts/adressList.ts
+++ src/consts/adressList.ts
@@ -1,3 +1,3 @@
-export let IS_DEVELOPMENT=1;
+export let IS_DEVELOPMENT=0;
 export const devServer = "http://localhost:8080";
 export const releaseServer = "https://personal-website-api-adnan.herokuapp.com"
