8bea2e036ad11b78377fb430f82b6b385052940e
AdnanCigtekin <cigtekinadnan@gmail.com>
2021-05-11 22:55:33 +0300

    feat: implemented loading bar and continuos wake up signals to server

--- src/components/GithubContribution.tsx
+++ src/components/GithubContribution.tsx
@@ -3,16 +3,46 @@ import React, { useEffect, useState } from "react";
 import { devServer, IS_DEVELOPMENT, releaseServer } from "../consts/adressList";
 import { GithubRequestInterface } from "../ınterfaces/GithubRequestInterface";
 import { CheckInViewport } from "../utility/CheckInViewport";
-import ReactAnime from 'react-animejs'
+import ReactAnime from "react-animejs";
+import ReactLoading from 'react-loading';
 
 export const GithubContribution = () => {
-const {Anime} = ReactAnime
+  const { Anime } = ReactAnime;
   const [contribObj, setContribObj] = useState({} as GithubRequestInterface);
-  const [contribCount, setContribCount] = useState(0);
+  const [contribCount, setContribCount] = useState(-1);
 
   const [scrolledTo, setScrolledTo] = useState(false);
   const [showPath, setShowPath] = useState(false);
 
+  const [backendAwake, setBackendAwake] = useState(false);
+
+
+
+  useEffect(() => {
+    if(backendAwake !== undefined){
+      const wakeInterval = setInterval(() => {
+        axios
+        .get(
+          IS_DEVELOPMENT === 1
+            ? `${devServer}/wake`
+            : `${releaseServer}/wake`
+        )
+        .then((response) =>{
+          if(response.status == 200){
+            setBackendAwake(true)
+            console.log("Server woke")
+            clearInterval(wakeInterval)
+          }
+        }
+    
+        )
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
+      },500)
+    }
+
+  }, [backendAwake]);
 
   useEffect(() => {
     const myItem = document.querySelector("#github-contribution");
@@ -26,60 +56,68 @@ const {Anime} = ReactAnime
   }, [scrolledTo]);
 
   useEffect(() => {
-    axios
-      .get(
-        IS_DEVELOPMENT === 1
-          ? `${devServer}/gh_scrape`
-          : `${releaseServer}/gh_scrape`
-      )
-      .then((response) =>
-        setContribObj(response.data as GithubRequestInterface)
-      )
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
-  }, []);
+    if (backendAwake) {
+      axios
+        .get(
+          IS_DEVELOPMENT === 1
+            ? `${devServer}/gh_scrape`
+            : `${releaseServer}/gh_scrape`
+        )
+        .then((response) =>
+          setContribObj(response.data as GithubRequestInterface)
+        )
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
+    }
+  }, [backendAwake]);
 
   useEffect(() => {
     if (contribObj !== undefined) {
       console.log(contribObj.contributionCount);
       console.log(contribObj.contributionCalendar);
       setContribCount(contribObj.contributionCount);
-
     }
   }, [contribObj]);
 
   return (
     <div>
       <div id="github-contribution"></div>
-      {showPath === false || contribCount === 0 || contribCount === undefined ? (
-        <div></div>
+      {showPath === false ||
+      contribCount === undefined ? (
+
+          <div className="github-panel">
+
+          <div style={{width:'30vh',height:'50%',margin:'auto'}}>
+            <ReactLoading type={"cylon"} color={"#ffffff"} width={"30vh"} height={"50%"} />
+
+          </div>
+        </div>
+        
+
       ) : (
         <div className="github-panel">
-
-
-            <div className="github-contrib-count">
+          <div className="github-contrib-count">
             <Anime
-            initial={[
+              initial={[
                 {
-                targets:"#contribCount",
-                innerText: [0, contribCount],
-                easing: "easeOutElastic(1, .1)",
-                round: true,
-                update: function(a) {
-                 
+                  targets: "#contribCount",
+                  innerText: [0, contribCount],
+                  easing: "easeOutElastic(1, .1)",
+                  round: true,
+                  update: function (a) {
                     const value = a.animations[0].currentValue;
                     document.querySelector("#contribCount").innerHTML = value;
-
+                  },
+                  duration: 3000,
                 },
-                duration: 3000
-            }
-            ]}
-            >
-                </Anime>
-                <h1><span id="contribCount">{contribCount}</span></h1> contributions in github in the last year!
-                </div>
-          
+              ]}
+            ></Anime>
+            <h1>
+              <span id="contribCount">{contribCount}</span>
+            </h1>{" "}
+            contributions in github in the last year!
+          </div>
         </div>
       )}
     </div>
