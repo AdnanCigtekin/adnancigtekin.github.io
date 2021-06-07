36d32131a44da8e4a19d067910e6b7baea7e4edf
AdnanCigtekin <cigtekinadnan@gmail.com>
2021-06-07 17:56:06 +0300

    fix: updated expired linkedin photos

--- src/consts/careerInformation.ts
+++ src/consts/careerInformation.ts
@@ -4,20 +4,20 @@ export const careerInformation = [
         "StartDate" : "March 2020",
         "EndDate" : "Ongoing",
         "Title" : "Assistant Software Engineer",
-        "LogoUrl" : "https://media-exp1.licdn.com/dms/image/C510BAQEtZ01_ey5nIA/company-logo_200_200/0/1586016478298?e=1622678400&v=beta&t=78I0EBdyYIx6avk5zGJsX0PxveeCthx8pXOqx3IuS-o"
+        "LogoUrl" : "https://media-exp3.licdn.com/dms/image/C510BAQEtZ01_ey5nIA/company-logo_200_200/0/1586016478298?e=1631145600&v=beta&t=-nwki6K5Nfn_eEonngH8vSTCinoePhSKu_CyoarZs1g"
     },
     {
         "Company" : "OBSS",
         "StartDate" : "July 2019",
         "EndDate" : "August 2019",
         "Title" : "Intern",
-        "LogoUrl" : "https://media-exp1.licdn.com/dms/image/C4D0BAQFntLUn_vWyVg/company-logo_200_200/0/1545652580525?e=1622678400&v=beta&t=CKNwRHdWwiQOvrztwle6cqXT3ebcAaYwlhlNePjDrZk"
+        "LogoUrl" : "https://media-exp3.licdn.com/dms/image/C4D0BAQFntLUn_vWyVg/company-logo_200_200/0/1545652580525?e=1631145600&v=beta&t=75n0XUWWMIYJJ3Hu7CSk4hNkpwo6-EBI4YLg5OD1tJ0"
     },
     {
         "Company" : "Ford Otosan",
         "StartDate" : "July 2018",
         "EndDate" : "August 2018",
         "Title" : "Intern",
-        "LogoUrl" : "https://media-exp1.licdn.com/dms/image/C4D0BAQGhkjcnLpiuVg/company-logo_200_200/0/1607669194222?e=1622678400&v=beta&t=GTOeOsF24suGfeaRpgxCijkxeY1UcszvD0UxVrXSauc"
+        "LogoUrl" : "https://media-exp3.licdn.com/dms/image/C4D0BAQGhkjcnLpiuVg/company-logo_200_200/0/1607669194222?e=1631145600&v=beta&t=n7AUj05K8bLUvl49BZdsb2uzrDFbFUsH5C4UCTtnJyQ"
     }
 ];
