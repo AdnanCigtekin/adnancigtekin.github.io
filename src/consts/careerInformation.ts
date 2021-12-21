cd793c519bca43402d22dac500e788290eec7506
AdnanCigtekin <cigtekinadnan@gmail.com>
2021-12-21 23:16:19 +0300

    feat: made icons of the companies provided within repo

--- src/consts/careerInformation.ts
+++ src/consts/careerInformation.ts
@@ -1,23 +1,28 @@
+import HuaweiIcon from "../resources/huawei-icon.jpeg"
+import ObssIcon from "../resources/obss-icon.jpeg"
+import OtosanIcon from "../resources/otosan-icon.jpeg"
+
+
 export const careerInformation = [
     {
         "Company" : "Huawei",
         "StartDate" : "March 2020",
         "EndDate" : "Ongoing",
         "Title" : "Assistant Software Engineer",
-        "LogoUrl" : "https://media-exp3.licdn.com/dms/image/C510BAQEtZ01_ey5nIA/company-logo_200_200/0/1586016478298?e=1631145600&v=beta&t=-nwki6K5Nfn_eEonngH8vSTCinoePhSKu_CyoarZs1g"
+        "LogoUrl" : HuaweiIcon
     },
     {
         "Company" : "OBSS",
         "StartDate" : "July 2019",
         "EndDate" : "August 2019",
         "Title" : "Intern",
-        "LogoUrl" : "https://media-exp3.licdn.com/dms/image/C4D0BAQFntLUn_vWyVg/company-logo_200_200/0/1545652580525?e=1631145600&v=beta&t=75n0XUWWMIYJJ3Hu7CSk4hNkpwo6-EBI4YLg5OD1tJ0"
+        "LogoUrl" : ObssIcon
     },
     {
         "Company" : "Ford Otosan",
         "StartDate" : "July 2018",
         "EndDate" : "August 2018",
         "Title" : "Intern",
-        "LogoUrl" : "https://media-exp3.licdn.com/dms/image/C4D0BAQGhkjcnLpiuVg/company-logo_200_200/0/1607669194222?e=1631145600&v=beta&t=n7AUj05K8bLUvl49BZdsb2uzrDFbFUsH5C4UCTtnJyQ"
+        "LogoUrl" : OtosanIcon
     }
 ];
